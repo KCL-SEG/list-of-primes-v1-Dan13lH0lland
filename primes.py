ad4802161858c8a1a2a3b16b63cd4ebc5d640f74
Daniel Holland <daniel.1.holland@kcl.ac.uk>
2022-10-02 10:40:39 +0100

    Adding code to determine if number is prime or not

--- primes.py
+++ primes.py
@@ -2,5 +2,26 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    list = [2]
+    counter = 3
+    baseCounter = 1
+    isPrime = False
+    if number_of_primes == 1:
+        list.append(2)
+    else:
+        while baseCounter != number_of_primes:
+            isPrime = False
+            for x in range(2, counter):
+                if (counter % x) == 0:
+                    isPrime = False
+                    break
+                else:
+                    isPrime = True
+            if isPrime:
+                list.append(counter)
+                baseCounter += 1
+            counter += 1
+    print(list)
+
+
+primes(10)
